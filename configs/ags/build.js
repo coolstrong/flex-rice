828c19e25ae8aa434397788aa125bb5a9fc8f286
Denis Sokolinskiy <eesokol22@gmail.com>
2024-03-06 14:39:56 +0700

    Tweaks after initial test

--- configs/ags/build.js
+++
@@ -1,9 +0,0 @@
-const output = await Bun.build({
-    entrypoints: ["./src/config.js"],
-    outdir: "./dist",
-    external: [`resource://*`, "gi://*"],
-});
-
-console.log(output.logs);
-
-export {};
--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-# mv ~/.config/hypr/ ~/.config/hypr-old
-# mv ~/.config/ags/ ~/.config/ags-old
-# mv ~/.config/wofi/ ~/.config/wofi-old
-# cp ~/.config/fish/config.fish ~/.config/fish/config.back.fish
 
 # copy files
+cp *.conf ~/.config/hypr/
+cp -r ./scripts ~/.config/hypr/scripts
 
-cp scripts *.conf ~/.config/hypr/
-cp -r ./configs/ags ~/.config/ags
+cp ./configs/ags/dist/config.js ~/.config/ags/
+cp -r ./configs/ags/assets ~/.config/ags/assets
+cp -r ./configs/ags/scripts ~/.config/ags/scripts
+cp -r ./configs/ags/scss ~/.config/ags/scss
+
 cp -r ./configs/wofi ~/.config/wofi
 
 # set permissions for scripts
@@ -18,32 +19,33 @@ sudo cp /etc/environment /etc/environmentOLD
 echo 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee -a /etc/environment
 
 # copy theme files
-mkdir ~/.local/share/color-schemes/
+mkdir -p ~/.local/share/color-schemes/
 cp ./configs/ags/modules/theme/plasma-colors/* ~/.local/share/color-schemes/
+mkdir -p ~/.config/qt5ct/
 cp ./configs/qt5ct.conf ~/.config/qt5ct/
 
 mkdir ~/.fonts
 cp -r ./configs/.fonts/* ~/.fonts
 
 mkdir ~/.local/share/icons
-tar -xzf ./configs/icons/BeautySolar.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Delight-brown-dark.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Gradient-Dark-Icons.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Infinity-Dark-Icons.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/kora-grey-light-panel.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Magma.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/NeonIcons.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/la-capitaine-icon-theme.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/oomox-aesthetic-dark.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Vivid-Dark-Icons.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Windows11-red-dark.tar.gz -C ~/.local/share/icons
-tar -xzf ./configs/icons/Zafiro-Nord-Dark-Black.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/BeautySolar.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Delight-brown-dark.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Gradient-Dark-Icons.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Infinity-Dark-Icons.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/kora-grey-light-panel.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Magma.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/NeonIcons.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/la-capitaine-icon-theme.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/oomox-aesthetic-dark.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Vivid-Dark-Icons.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Windows11-red-dark.tar.gz -C ~/.local/share/icons
+tar -xf ./configs/icons/Zafiro-Nord-Dark-Black.tar.gz -C ~/.local/share/icons
 
 mkdir ~/.themes
-tar -xzf ./configs/gtk-themes/Cabinet-Light-Orange.tar.gz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Kimi-dark.tar.gz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Nordic-darker-standard-buttons.tar.gz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Orchis-Green-Dark-Compact.tar.gz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Shades-of-purple.tar.xz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Tokyonight-Dark-BL.tar.gz -C ~/.themes
-tar -xzf ./configs/gtk-themes/Dracula.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Cabinet-Light-Orange.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Kimi-dark.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Nordic-darker-standard-buttons.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Orchis-Green-Dark-Compact.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Shades-of-purple.tar.xz -C ~/.themes
+tar -xf ./configs/gtk-themes/Tokyonight-Dark-BL.tar.gz -C ~/.themes
+tar -xf ./configs/gtk-themes/Dracula.tar.gz -C ~/.themes
